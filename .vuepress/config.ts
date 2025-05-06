24d2d25a562369e3828c5bf0631abe55c9210a12
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-05-07 07:29:42 +0800

    chore: icon

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -7,7 +7,6 @@ export default defineUserConfig({
   title: "MSLX",
   description: "MSLX-新一代跨平台服务器管理/联机工具",
   bundler: viteBundler(),
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   lang: "zh-CN",
   // bundler: webpackBundler(),
   theme: recoTheme({
